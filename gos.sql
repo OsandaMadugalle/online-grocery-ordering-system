fc06795d2cf459492ec23bc257da0d2014334671
osandamadugalle <osandamadugalle@gmail.com>
2025-03-09 11:55:12 +0530

    Update gos.sql

--- gos.sql
+++ gos.sql
@@ -1,17 +1,20 @@
-create database gos;
+CREATE DATABASE gos;
 
-create table admin(
-	admin_id int auto_increment primary key,
-    username varchar(50),
-    first_name varchar(50),
-    last_name varchar(50),
-    phone varchar(50),
-    email varchar(50),
-    password varchar(50)
+Use gos;
+
+CREATE TABLE Admin(
+    Admin_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(50),
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50),
+    Phone VARCHAR(50),
+    Email VARCHAR(50),
+    Password VARCHAR(50)
 );
 
-insert into admin
-values(0,"Admin","Osanda","Madugalle","+94705531088","osandamadugalle@gmail.com","admin123"),
-	(0,"test","Saman","Silva","+94705555555","saman@gmail.com","test@123");
+INSERT INTO Admin (Admin_ID, Username, First_Name, Last_Name, Phone, Email, Password)
+VALUES
+    (0, "Admin", "Osanda", "Madugalle", "+94705531088", "osandamadugalle@gmail.com", "admin123"),
+    (0, "Test", "Saman", "Silva", "+94705555555", "saman@gmail.com", "test@123");
 
-select * from admin;
+SELECT * FROM Admin;
