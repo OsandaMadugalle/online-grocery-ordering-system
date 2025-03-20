c9e93387d466a3aca7d0b7a3fffed3614025ed34
osandamadugalle <osandamadugalle@gmail.com>
2025-03-20 21:29:22 +0530

    java version updates

--- src/main/java/com/gos/util/ConnectionData.java
+++ src/main/java/com/gos/util/ConnectionData.java
@@ -5,5 +5,5 @@ public class ConnectionData {
 
     public static final String DATABASE = "gos"; // Name of the database
     public static final String USERNAME = "root";  // Database username
-    public static final String PASSWORD = "";// Database password
+    public static final String PASSWORD = "admin";// Database password
 }
