46845e4a56157a46a92afe3dab4db690f6e0ab8d
osandamadugalle <osandamadugalle@gmail.com>
2025-04-14 18:32:55 +0530

    Update gos.sql

--- gos.sql
+++ gos.sql
@@ -12,9 +12,20 @@ CREATE TABLE Admin(
     Password VARCHAR(50)
 );
 
-INSERT INTO Admin (Admin_ID, Username, First_Name, Last_Name, Phone, Email, Password)
+INSERT INTO  InventoryManager (M_ID, Username, First_Name, Last_Name, Phone, Email, Password)
 VALUES
     (0, "Admin", "Osanda", "Madugalle", "+94705531088", "osandamadugalle@gmail.com", "admin123"),
     (0, "Test", "Saman", "Silva", "+94705555555", "saman@gmail.com", "test@123");
 
-SELECT * FROM Admin;
+SELECT * FROM admin;
+SELECT * FROM InventoryManager;
+
+CREATE TABLE InventoryManager(
+    M_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(50) UNIQUE,
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50),
+    Phone VARCHAR(50),
+    Email VARCHAR(50),
+    Password VARCHAR(50)
+);
