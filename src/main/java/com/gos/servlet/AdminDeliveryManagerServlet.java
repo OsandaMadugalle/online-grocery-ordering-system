b4809dc7e3701bd0f5634770d84b42b7cd2c9011
osandamadugalle <osandamadugalle@gmail.com>
2025-04-20 15:48:34 +0530

    Create AdminDeliveryManagerServlet.java

---
+++ src/main/java/com/gos/servlet/AdminDeliveryManagerServlet.java
@@ -0,0 +1,42 @@
+package com.gos.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.gos.model.DeliveryManager;
+import com.gos.service.DeliveryManagerService;
+
+
+@WebServlet("/AdminDeliveryManagerServlet")
+public class AdminDeliveryManagerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		try {
+			DeliveryManagerService service = new DeliveryManagerService();
+			ArrayList<DeliveryManager> deliveryManager = service.getAllDelivaryManager();
+			
+			request.setAttribute("deliveryManager", deliveryManager);
+			RequestDispatcher dis = request.getRequestDispatcher("admin/manageDeliveryManagers.jsp");
+			dis.forward(request, response);
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
