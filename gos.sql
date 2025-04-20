5ce5a0e8d622d9968c30db248db1f547e7eea63e
osandamadugalle <osandamadugalle@gmail.com>
2025-04-20 15:48:03 +0530

    Update gos.sql

--- gos.sql
+++ gos.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE gos;
-
 Use gos;
 
 CREATE TABLE Admin(
@@ -12,14 +11,6 @@ CREATE TABLE Admin(
     Password VARCHAR(50)
 );
 
-INSERT INTO  InventoryManager (M_ID, Username, First_Name, Last_Name, Phone, Email, Password)
-VALUES
-    (0, "Admin1", "Osanda", "Madugalle", "+94705531088", "osandamadugalle@gmail.com", "admin123"),
-    (0, "Test1", "Saman", "Silva", "+94705555555", "saman@gmail.com", "test@123");
-
-SELECT * FROM admin;
-SELECT * FROM InventoryManager;
-
 CREATE TABLE InventoryManager(
     M_ID INT AUTO_INCREMENT PRIMARY KEY,
     Username VARCHAR(50) UNIQUE,
@@ -29,3 +20,23 @@ CREATE TABLE InventoryManager(
     Email VARCHAR(50),
     Password VARCHAR(50)
 );
+
+CREATE TABLE DeliveryManager(
+    D_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(50) UNIQUE,
+    First_Name VARCHAR(50),
+    Last_Name VARCHAR(50),
+    Phone VARCHAR(50),
+    Email VARCHAR(50),
+    Password VARCHAR(50)
+);
+
+INSERT INTO  DeliveryManager (D_ID, Username, First_Name, Last_Name, Phone, Email, Password)
+VALUES
+    (0, "Admin1", "Osanda", "Madugalle", "+94705531088", "osandamadugalle@gmail.com", "admin123"),
+    (0, "Test1", "Saman", "Silva", "+94705555555", "saman@gmail.com", "test@123");
+
+SELECT * FROM admin;
+SELECT * FROM InventoryManager;
+SELECT * FROM DeliveryManager;
+
