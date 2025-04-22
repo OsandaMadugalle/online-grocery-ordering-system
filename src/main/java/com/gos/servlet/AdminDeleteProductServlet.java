2b06063b4e0fdba06bcc515ea1140e2261d3545c
osandamadugalle <osandamadugalle@gmail.com>
2025-04-22 15:06:56 +0530

    Create AdminDeleteProductServlet.java

---
+++ src/main/java/com/gos/servlet/AdminDeleteProductServlet.java
@@ -0,0 +1,33 @@
+package com.gos.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import com.gos.model.Product;
+import com.gos.service.ProductService;
+
+@WebServlet("/AdminDeleteProductServlet")
+public class AdminDeleteProductServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response); // Delegate GET to POST
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Product product = new Product();
+        product.setId(Integer.parseInt(request.getParameter("id")));
+
+        ProductService productService = new ProductService();
+        productService.deleteProduct(product);
+
+        response.sendRedirect(request.getContextPath() + "/manageProducts");
+    }
+}
