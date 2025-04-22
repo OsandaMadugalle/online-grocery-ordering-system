ad7988488e1bbeedcb2acad35c501e9bddee735f
osandamadugalle <osandamadugalle@gmail.com>
2025-04-22 06:29:43 +0530

    Create InventoryManagerUpdateServlet.java

---
+++ src/main/java/com/gos/servlet/InventoryManagerUpdateServlet.java
@@ -0,0 +1,43 @@
+package com.gos.servlet;import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.util.List;
+
+import com.gos.model.Admin;
+import com.gos.model.InventoryManager;
+import com.gos.service.AdminService;
+import com.gos.service.InventoryManagerService;
+
+
+@WebServlet("/InventoryManagerUpdateServlet")
+public class InventoryManagerUpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    String id = request.getParameter("m_id");
+	    String username = request.getParameter("username");
+	    String fName = request.getParameter("firstName");
+	    String lName = request.getParameter("lastName");
+	    String phone = request.getParameter("phone");
+	    String email = request.getParameter("email");
+	    String password = request.getParameter("password");
+
+	    boolean isTrue = InventoryManagerService.updateInventoryManager(id, username, fName, lName, phone, email, password);
+
+	    if (isTrue) {	       
+	        List<InventoryManager> updatedInventoryManagerDetails = InventoryManagerService.getInventoryManagerById(id);
+	        
+	        HttpSession session = request.getSession();
+	        session.setAttribute("inventoryMangerDetails", updatedInventoryManagerDetails);
+
+	        response.sendRedirect(request.getContextPath() + "/inventoryManager/inventoryManagerDashboard.jsp");
+	    } else {
+	        response.sendRedirect(request.getContextPath() + "/inventoryManager/updateInventoryManagerProfile.jsp");
+	    }
+	}
+
+}
