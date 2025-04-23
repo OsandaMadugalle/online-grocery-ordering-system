f11dbbe985d78f4da01b492ef1ce73170a0c16c1
osandamadugalle <osandamadugalle@gmail.com>
2025-04-23 08:59:57 +0530

    Create Customer.java

---
+++ src/main/java/com/gos/model/Customer.java
@@ -0,0 +1,82 @@
+package com.gos.model;
+
+public class Customer {
+
+    private int customer_id;
+    private String username;
+    private String first_name;
+    private String last_name;
+    private String phone;
+    private String email;
+    private String address;
+
+    public Customer(int customer_id, String username, String first_name, String last_name, String phone, String email, String address) {
+        this.customer_id = customer_id;
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Customer() {
+        
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
