5430515feb4e3180b56e63948d55c02eb03c97af
osandamadugalle <osandamadugalle@gmail.com>
2025-04-23 10:25:24 +0530

    Update CustomerLogoutServlet.java

--- src/main/java/com/gos/servlet/CustomerLogoutServlet.java
+++ src/main/java/com/gos/servlet/CustomerLogoutServlet.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet("/customerLogoutServlet")
+@WebServlet("/CustomerLogoutServlet")
 public class CustomerLogoutServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     
