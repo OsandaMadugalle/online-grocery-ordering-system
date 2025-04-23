0811e3e745f850a4691fe627204bf7980c0d3ab7
osandamadugalle <osandamadugalle@gmail.com>
2025-04-23 11:22:22 +0530

    Update CustomerService.java

--- src/main/java/com/gos/service/CustomerService.java
+++ src/main/java/com/gos/service/CustomerService.java
@@ -71,4 +71,63 @@ public class CustomerService {
 	            rs.getString("Password") // Updated field from 'address' to 'password'
 	    );
 	}
+	
+	//update cus
+	public static boolean updateCustomer(String id, String username, String fname, String lname, String phone, String email, String password) {
+	    boolean isSuccess = false;
+	    String sql = "UPDATE customer SET Username=?, First_Name=?, Last_Name=?, Phone=?, Email=?, Password=? WHERE Customer_ID=?";
+
+	    try (Connection conn = DBConnection.getConnection();
+	         PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+	        stmt.setString(1, username);
+	        stmt.setString(2, fname);
+	        stmt.setString(3, lname);
+	        stmt.setString(4, phone);
+	        stmt.setString(5, email);
+	        stmt.setString(6, password);
+	        stmt.setInt(7, Integer.parseInt(id)); 
+
+	        int rs = stmt.executeUpdate();
+
+	        isSuccess = rs > 0;
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return isSuccess;
+	}
+	
+	//get by id
+	public static List<Customer> getCustomerById(String id) {
+    	
+	    List<Customer> customers = new ArrayList<>();
+	    String sql = "SELECT * FROM customer WHERE Customer_ID = ?";
+
+	    try (Connection conn = DBConnection.getConnection();
+	         PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+	        stmt.setInt(1, Integer.parseInt(id));
+	        
+	        ResultSet rs = stmt.executeQuery(); 
+	        
+	        while (rs.next()) {
+	            int cId = rs.getInt(1);
+	            String username = rs.getString(2);
+	            String fName = rs.getString(3);
+	            String lName = rs.getString(4);
+	            String phone = rs.getString(5);
+	            String email = rs.getString(6);
+	            String password = rs.getString(7);
+
+	            Customer customer = new Customer(cId, username, fName, lName, phone, email, password);
+	            customers.add(customer);
+	        }
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+
+	    return customers;
+	}
 }
