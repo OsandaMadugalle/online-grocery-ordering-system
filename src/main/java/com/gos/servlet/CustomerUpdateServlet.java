923f8056c68c8280f97c3a2e8cb73d37af7fe721
osandamadugalle <osandamadugalle@gmail.com>
2025-04-23 17:26:12 +0530

    Update CustomerUpdateServlet.java

--- src/main/java/com/gos/servlet/CustomerUpdateServlet.java
+++ src/main/java/com/gos/servlet/CustomerUpdateServlet.java
@@ -21,8 +21,8 @@ public class CustomerUpdateServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String id = request.getParameter("customer_id");
         String username = request.getParameter("username");
-        String fName = request.getParameter("first_name");  // was "firstName"
-        String lName = request.getParameter("last_name");   // was "lastName"
+        String fName = request.getParameter("first_name");  
+        String lName = request.getParameter("last_name");   
         String phone = request.getParameter("phone");
         String email = request.getParameter("email");
         String password = request.getParameter("password");
@@ -35,20 +35,12 @@ public class CustomerUpdateServlet extends HttpServlet {
             HttpSession session = request.getSession();
             session.setAttribute("customerDetails", updatedCustomerDetails);
 
-            //request.getRequestDispatcher("/customer/customerAccount.jsp").forward(request, response);
             response.sendRedirect(request.getContextPath() + "/customer/customerAccount.jsp");
         } else {
             response.sendRedirect(request.getContextPath() + "/customer/updateCustomerProfile.jsp");
         }
     }
     
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // logic for handling GET requests (e.g., show customer profile page)
-        String customerId = request.getParameter("customer_id");
-        List<Customer> customer = CustomerService.getCustomerById(customerId);
-        request.setAttribute("customer", customer);
-        request.getRequestDispatcher("/customer/customerAccount.jsp").forward(request, response);
-    }
+    
 
 }
