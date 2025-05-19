8a5847326efc48b22a39b4425171b7b4ea735eeb
osandamadugalle <osandamadugalle@gmail.com>
2025-05-20 04:46:42 +0530

    Update AdminCreateInventoryManagerServlet.java

--- src/main/java/com/gos/servlet/AdminCreateInventoryManagerServlet.java
+++ src/main/java/com/gos/servlet/AdminCreateInventoryManagerServlet.java
@@ -16,23 +16,28 @@ public class AdminCreateInventoryManagerServlet extends HttpServlet {
 private static final long serialVersionUID = 1L;
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		 String username = request.getParameter("username");
-		 String first_name  = request.getParameter("firstName");
-		 String last_name  = request.getParameter("lastName");
-		 String phone  = request.getParameter("phone");
-		 String email  = request.getParameter("email");
-		 String password  = request.getParameter("password");
-		 
-		 boolean isTrue;
-		 
-		 isTrue= InventoryManagerService.addInventoryManager(username, first_name, last_name, phone, email, password);
-		 
-		 if(isTrue==true) {
-			 System.out.println("Create Inventory Manager Success");
-			 response.sendRedirect("manageInventoryManager");
-		 }else {
-			 System.out.println("Create Inventory Manager Failed");
-			 response.sendRedirect(request.getContextPath() + "/admin/createInventoryManager.jsp");
-		 }
+	    String username = request.getParameter("username");
+	    String first_name = request.getParameter("firstName");
+	    String last_name = request.getParameter("lastName");
+	    String phone = request.getParameter("phone");
+	    String email = request.getParameter("email");
+	    String password = request.getParameter("password");
+	
+	    try {
+	        boolean isTrue = InventoryManagerService.addInventoryManager(username, first_name, last_name, phone, email, password);
+	
+	        if (isTrue) {
+	            System.out.println("Create Inventory Manager Success");
+	            response.sendRedirect("manageInventoryManager");
+	        } else {
+	            System.out.println("Create Inventory Manager Failed");
+	            response.sendRedirect(request.getContextPath() + "/admin/createInventoryManager.jsp");
+	        }
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        System.out.println("Exception occurred while creating inventory manager: " + e.getMessage());
+	        response.sendRedirect(request.getContextPath() + "/admin/createInventoryManager.jsp?error=true");
+	    }
 	}
+
 }
